d98552f20ee3d01200251b30ea34affc0a2a92d6
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-06-16 19:27:10 -0700

    Update index.js
    reversed string solved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reveresd = '';
+
+    for (let character of str) {
+
+        reveresd = character + reveresd;
+    }
+    return reveresd
+}
 
 module.exports = reverse;
+
+
+// What is coming In?
+// the string 'str' is being passed in
+// Once in the function we need to reverse the string
+
+//create a empty str for the reversed variable 
+
+// iterate over the str string to  in return send the reversed elements to the empty reveresed string
+
+// so the empty revered string  equals the 'chacter of the str string' + the reveresed str
+
+// reveresed = ('apple') + ('')
+// revered = ('appl') + ('e')
+// reversed = ('app') + ('el')
+
+
+
+// What is coming out?
+//reversed version of the 'stg' string
